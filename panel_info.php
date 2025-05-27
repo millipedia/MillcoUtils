ee43b984405c3bbbabb985580493601e3746b306
Stephen <stephen@millipedia.co.uk>
2025-05-27 12:30:41 +0100

    Formatting.

--- panel_info.php
+++ panel_info.php
@@ -2,13 +2,11 @@
 
 namespace ProcessWire;
 
-
 /**
  * Template file which is included in the top panel
  * in the admin.
  * Handy for project links and the like.
  */
-
 
 
 echo '<div class="uk-grid-small" uk-grid>';
